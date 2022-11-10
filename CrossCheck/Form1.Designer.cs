63d23383ae624f2c7cfad86a695d09a516d165b0
BuildTools <unconfigured@null.spigotmc.org>
2022-11-09 23:31:40 -0500

    Add project files.

---
+++ CrossCheck/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace CrossCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.BrowseFilesButton = new System.Windows.Forms.Button();
+            this.FilePathText = new System.Windows.Forms.TextBox();
+            this.ResultsBox = new System.Windows.Forms.RichTextBox();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BrowseFilesButton
+            // 
+            this.BrowseFilesButton.Location = new System.Drawing.Point(12, 12);
+            this.BrowseFilesButton.Name = "BrowseFilesButton";
+            this.BrowseFilesButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseFilesButton.TabIndex = 0;
+            this.BrowseFilesButton.Text = "Browse";
+            this.BrowseFilesButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.BrowseFilesButton.UseVisualStyleBackColor = true;
+            this.BrowseFilesButton.Click += new System.EventHandler(this.BrowseFilesButton_Click);
+            // 
+            // FilePathText
+            // 
+            this.FilePathText.Location = new System.Drawing.Point(93, 12);
+            this.FilePathText.Name = "FilePathText";
+            this.FilePathText.ReadOnly = true;
+            this.FilePathText.Size = new System.Drawing.Size(302, 23);
+            this.FilePathText.TabIndex = 1;
+            // 
+            // ResultsBox
+            // 
+            this.ResultsBox.Location = new System.Drawing.Point(12, 41);
+            this.ResultsBox.Name = "ResultsBox";
+            this.ResultsBox.ReadOnly = true;
+            this.ResultsBox.Size = new System.Drawing.Size(383, 291);
+            this.ResultsBox.TabIndex = 2;
+            this.ResultsBox.Text = "";
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(167, 338);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(75, 23);
+            this.CheckButton.TabIndex = 3;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 371);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.ResultsBox);
+            this.Controls.Add(this.FilePathText);
+            this.Controls.Add(this.BrowseFilesButton);
+            this.Name = "Form1";
+            this.Text = "Inventory Cross Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button BrowseFilesButton;
+        private System.Windows.Forms.TextBox FilePathText;
+        private System.Windows.Forms.RichTextBox ResultsBox;
+        private System.Windows.Forms.Button CheckButton;
+    }
+}
+
---
+++ CrossCheck/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CrossCheck
+{
+    public partial class Form1 : Form
+    {
+
+        private static Excel.Workbook ExcelBook;
+        private static Excel.Application ExcelApp;
+        private static Excel.Worksheet InventorySheet;
+        private static Excel.Worksheet SurplusSheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Define File Dialog Properties
+            openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Title = "Browse Excel Files";
+            openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog.DefaultExt = "xlsx";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.FileName = "";
+
+            //Define Excel Application Properties
+            ExcelApp = new Excel.Application();
+            ExcelApp.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BrowseFilesButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FilePathText.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            //Open Workbook for compare
+            ExcelBook = ExcelApp.Workbooks.Open(FilePathText.Text);
+
+            //Open Inventory and Surplus sheets
+            InventorySheet = ExcelBook.Worksheets["Inventory"];
+            SurplusSheet = ExcelBook.Worksheets["Surplus"];
+
+            //Find Serial Columns & Row Ranges for each sheet
+            String InventorySerialColumn = GetSerialRow(InventorySheet);
+            String SurplusSerialColumn = GetSerialRow(SurplusSheet);
+
+            int InventorySerialLastRow = InventorySheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;// Find last row with data
+            int SurplusSerialLastRow = SurplusSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+
+            //Put serials for each sheet into a Set
+            HashSet<String> InventorySerials = new HashSet<String>(((System.Array)
+                InventorySheet.get_Range(InventorySerialColumn + "2", InventorySerialColumn + InventorySerialLastRow).Cells.Value).OfType<object>().Select(o => o.ToString()).ToArray());
+            HashSet<String> SurplusSerials = new HashSet<String>(((System.Array) 
+                SurplusSheet.get_Range(SurplusSerialColumn + "2", SurplusSerialColumn + SurplusSerialLastRow).Cells.Value).OfType<object>().Select(o => o.ToString()).ToArray());
+            //getting the range out of excel returns a System.Array which isn't able to be converted to a HashSet,
+            //so LINQ must be used to convert to a normal array
+
+            //Find the intersection of sets
+            HashSet<String> SerialResults = new HashSet<String>(InventorySerials);
+            SerialResults.IntersectWith(SurplusSerials);
+
+            //Display results
+            foreach (String serial in SerialResults)
+                ResultsBox.Text += serial + " ";
+        }
+
+        private String GetSerialRow(Excel.Worksheet worksheet)
+        {
+            //Find last colum and put first row into list to find the "Serial" Column
+            String lastCol = ((char)(worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Column + 64)).ToString();
+            Array Columns = (System.Array)worksheet.get_Range("A1", lastCol + "1").Cells.Value;
+
+            //Return Serial Column Character
+            for (int i = 1; i < Columns.Length; i++)
+                if (Columns.GetValue(1, i).ToString().ToLower().Contains("serial"))
+                    return ((char)(i + 64)).ToString();// add 65 and convert to char to change column numerical value to character
+
+            return null;
+        }
+    }
+}
